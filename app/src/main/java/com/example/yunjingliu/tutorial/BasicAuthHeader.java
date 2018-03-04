17e16b443dc181a61f9610be6a32487b239e9e46
Yunjing Liu <yunjingliu@buaa.edu.cn>
2018-03-04 17:29:51 -0500

    dsfg

--- app/src/main/java/com/example/yunjingliu/tutorial/BasicAuthHeader.java
+++ app/src/main/java/com/example/yunjingliu/tutorial/BasicAuthHeader.java
@@ -4,6 +4,7 @@ import android.util.Base64;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Response;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONObject;
@@ -15,20 +16,13 @@ import java.util.Map;
  * Created by YunjingLiu on 3/4/18.
  */
 
-public class JsonAuthRequest extends JsonObjectRequest {
+public class BasicAuthHeader {
     private final String username;
     private final String password;
-    public JsonAuthRequest(int method, String url, Map<String, String> map, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
-        super(method, url, new JSONObject(map), listener, errorListener);
-        this.username = null;
-        this.password = null;
-    }
-    public JsonAuthRequest(int method, String url, Map<String, String> map, String username, String password, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
-        super(method, url, new JSONObject(map), listener, errorListener);
+    public BasicAuthHeader(String username, String password) {
         this.username = username;
         this.password = password;
     }
-    @Override
     public Map<String, String> getHeaders() throws AuthFailureError {
         Map<String, String> params = new HashMap<>();
         params.put("Content-Type", "application/json");
