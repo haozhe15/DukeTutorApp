ac899ec9156240ce937e873606c7c2bda9bad3e7
Haozhe Wang <hw189@duke.edu>
2018-03-04 20:48:29 -0500

    tutor session post update

--- app/src/main/java/com/example/yunjingliu/tutorial/TutorSessionPostActivity.java
+++ app/src/main/java/com/example/yunjingliu/tutorial/TutorSessionPostActivity.java
@@ -3,11 +3,26 @@ package com.example.yunjingliu.tutorial;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
 public class TutorSessionPostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_session_post);
+
+        Spinner spinner = (Spinner) findViewById(R.id.date_choose);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.week_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
+
+
 }
