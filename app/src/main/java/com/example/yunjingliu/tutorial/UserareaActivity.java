01ad3390e420ce1280c8d1ee78503fdf0ed89f98
Haozhe Wang <hw189@duke.edu>
2018-03-04 19:51:07 -0500

    update 04

--- app/src/main/java/com/example/yunjingliu/tutorial/UserareaActivity.java
+++ app/src/main/java/com/example/yunjingliu/tutorial/UserareaActivity.java
@@ -1,7 +1,9 @@
 package com.example.yunjingliu.tutorial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class UserareaActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class UserareaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userarea);
+    }
+
+    public void addNew(View view){
+        Intent intent = new Intent(this, TutorSessionPostActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void skipToSearch(View view){
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
+
     }
 }
