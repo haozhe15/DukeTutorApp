fae0352ed399f872ee152fd54bfd7796b553a3e7
Zhang Rui <zrui16@hotmail.com>
2018-03-13 17:07:35 -0400

    Add RFC reference in BasicAuthProvider.

--- app/src/main/java/com/example/yunjingliu/tutorial/BasicAuthProvider.java
+++ app/src/main/java/com/example/yunjingliu/tutorial/BasicAuthProvider.java
@@ -10,6 +10,8 @@ class BasicAuthProvider implements AuthProvider {
     private String authorization;
 
     public void setUsernamePassword(String username, String password) {
+        // HTTP Basic authentication. See:
+        // https://tools.ietf.org/html/rfc2617#section-2
         String userpass = username + ":" + password;
         authorization = "Basic " + Base64.encodeToString(userpass.getBytes(), Base64.NO_WRAP);
     }
