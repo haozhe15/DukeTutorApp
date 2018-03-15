8724d7a3575e8c4ebb192ac620ba2ace40ce5b14
Haozhe Wang <hw189@duke.edu>
2018-03-15 18:26:35 -0400

    search 01

--- app/src/main/java/com/example/yunjingliu/tutorial/SearchActivity.java
+++ app/src/main/java/com/example/yunjingliu/tutorial/SearchActivity.java
@@ -1,13 +1,28 @@
 package com.example.yunjingliu.tutorial;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchableActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
     }
 }
