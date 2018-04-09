b429c7c9f21eadf3433687cdce9c044622aaf94a
Haozhe Wang <hw189@duke.edu>
2018-04-09 16:26:27 -0400

    public RegistrationActivity() {         form = new JsonForm(this, new ErrorListener(this));     }rivate final JsonForm form;
        public RegistrationActivity() {
            form = new JsonForm(this, new ErrorListener(this));
        }rivate final JsonForm form;
    
        public RegistrationActivity() {
            form = new JsonForm(this, new ErrorListener(this));
        }

--- app/src/main/java/com/example/yunjingliu/tutorial/FeedbackActivity.java
+++ app/src/main/java/com/example/yunjingliu/tutorial/FeedbackActivity.java
@@ -2,12 +2,33 @@ package com.example.yunjingliu.tutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
 
+import com.zr.forms.JsonForm;
+
+/**
+ * Created by Haozhe Wang on 04/08/18.
+ */
 public class FeedbackActivity extends AppCompatActivity {
+    private final JsonForm form;
+
+    public FeedbackActivity() {
+        form = new JsonForm(this, new ErrorListener(this));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        RatingBar simpleRatingBar = (RatingBar) findViewById(R.id.ratingBar); // initiate a rating bar
+        simpleRatingBar.setRating((float) 3.5); // set default rating
+        Float ratingNumber = simpleRatingBar.getRating(); // get rating number from a rating bar
+
+    }
+
+    public void onClickSubmit(View view){
+
     }
 }
