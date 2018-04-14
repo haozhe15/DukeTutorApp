a8c11464dbd1ac7f38a9b79b3fa6732f10768f7e
jw573 <jw573@duke.edu>
2018-04-14 10:32:56 -0400

    message competed, add code for null application

---
+++ app/src/test/java/com/example/yunjingliu/tutorial/LoginActivityTest.java
@@ -0,0 +1,32 @@
+package com.example.yunjingliu.tutorial;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by MyPC on 2018/4/10.
+ */
+public class LoginActivityTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+    }
+
+    @Test
+    public void onClickLogin() throws Exception {
+    }
+
+    @Test
+    public void onLoginSuccess() throws Exception {
+    }
+
+    @Test
+    public void onClickRegister() throws Exception {
+    }
+
+}
